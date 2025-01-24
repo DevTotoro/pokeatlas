c0d7e876808a08ccc81a3940e471edc8ece8db40
devtotoro <totoro.softwaredev@gmail.com>
2025-01-24 01:42:28 +0000

    Create filterable Pokemon list

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -2,27 +2,30 @@ import { Flex, Container } from '@radix-ui/themes';
 
 import { Title } from '~/components/title';
 import { SearchForm } from '~/components/search-form';
+import { PokemonList } from '~/components/pokemon-list';
 
 interface Props {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-const PokemonList = async ({ searchParams }: Props) => {
+const PokemonListPage = async ({ searchParams }: Props) => {
   const { search } = await searchParams;
 
   const processedSearch = Array.isArray(search) ? search.join(',') : search;
 
   return (
     <Container size='2' px='6' py='4'>
-      <Flex direction='column' gap='5'>
+      <Flex direction='column' gap='8'>
         <Flex justify='between' align='center' gap='4'>
           <Title size='small' isLink />
 
           <SearchForm size='small' defaultSearch={processedSearch} />
         </Flex>
+
+        <PokemonList search={processedSearch} />
       </Flex>
     </Container>
   );
 };
 
-export default PokemonList;
+export default PokemonListPage;
